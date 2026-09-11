a50b5f087be34f2e3ace14424062740e9b9d6396
impalichahal <impalichahal@gmail.com>
2026-09-11 15:27:42 +0530

    Update website

--- src/config/siteConfig.js
+++ src/config/siteConfig.js
@@ -2,17 +2,17 @@ export const COMPANY_NAME = 'Rosey Lanscaping'
 export const TAGLINE = 'Premium outdoor spaces, thoughtfully designed.'
 export const BUSINESS_PHONE = '+1 (236) 883-1316'
 export const EMAIL = 'roseylandscaping@gmail.com'
-export const ADDRESS = '245 Cedar Valley Drive, Suite 200, Austin, TX 78701'
+export const ADDRESS = '13027 98 Ave, Surrey BC, V3T 1B6 Canada'
 export const WHATSAPP_NUMBER = '1(236) 883-1316'
 export const BUSINESS_HOURS = 'Mon - Sat: 8:00 AM - 6:00 PM'
-export const GOOGLE_MAP_URL = 'https://www.google.com/maps?q=245%20Cedar%20Valley%20Drive%20Austin%20TX&output=embed'
+export const GOOGLE_MAP_URL = 'https://maps.app.goo.gl/vjhsQWTyd7S7Mivb9'
 export const FORM_ENDPOINT = 'https://formspree.io/f/your-form-id'
 
 export const SOCIAL_LINKS = {
   facebook: 'https://www.facebook.com',
-  instagram: 'https://www.instagram.com',
-  linkedin: 'https://www.linkedin.com',
-  pinterest: 'https://www.pinterest.com',
+  instagram: 'https://www.instagram.com/roseylandscaping',
+  // linkedin: 'https://www.linkedin.com',
+  // pinterest: 'https://www.pinterest.com',
 }
 
 export const HOME_META = {
